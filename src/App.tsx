df63629202ae6043c2c9d89053d2e3ae993c44f2
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2025-02-11 21:14:45 -0300

    chore: install Phosphor Icons and add background images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import topCover from './assets/top-cover.png'
+import bottomCover from './assets/bottom-cover.png'
+import { Heart } from '@phosphor-icons/react'
+
 export function App() {
   return (
-    <div className="text-gray-600 font-maven">Hello world!</div>
+    <div>
+      <img src={topCover} className='w-full' alt="top cover" />
+      <div className="text-gray-600 font-maven">Hello world!</div>
+      <Heart />
+      <img src={bottomCover} className='w-full' alt="bottom cover" />
+    </div>
   )
 }
