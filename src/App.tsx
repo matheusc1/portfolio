c3e4f1d2dcbafcd8a10d9a1bbdfbfcf83c632364
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2025-02-14 21:32:51 -0300

    chore: configure Biome for linting

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import { Heart } from '@phosphor-icons/react'
 export function App() {
   return (
     <div>
-      <img src={topCover} className='w-full' alt="top cover" />
+      <img src={topCover} className="w-full" alt="top cover" />
       <div className="text-gray-600 font-maven">Hello world!</div>
       <Heart />
-      <img src={bottomCover} className='w-full' alt="bottom cover" />
+      <img src={bottomCover} className="w-full" alt="bottom cover" />
     </div>
   )
 }
