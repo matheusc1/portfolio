da4c7c91284598efb44bfd07a322f3c0b3d0c839
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2025-02-14 22:12:41 -0300

    feat: add header description

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import topCover from './assets/top-cover.png'
-import bottomCover from './assets/bottom-cover.png'
-import { Heart } from '@phosphor-icons/react'
 
 export function App() {
   return (
-    <div>
-      <img src={topCover} className="w-full" alt="top cover" />
-      <div className="text-gray-600 font-maven">Hello world!</div>
-      <Heart />
-      <img src={bottomCover} className="w-full" alt="bottom cover" />
+    <div className="h-screen w-full">
+      <div
+        className="bg-cover bg-center h-full w-full flex flex-col items-center justify-center gap-5"
+        style={{ backgroundImage: `url(${topCover})` }}
+      >
+        <div className="space-y-2 text-center">
+          <p className="text-gray-200 font-subtitle text-xl">
+            Hello! Meu nome é <span className="text-red">Matheus Cardoso</span>{' '}
+            e sou
+          </p>
+          <h1 className="text-gray-100 font-title text-5xl">
+            Desenvolvedor Front-End
+          </h1>
+        </div>
+        <span className="text-gray-300 w-[660px] text-sm text-center">
+          Transformo necessidades em aplicações reais, evolventes e funcionais.
+          Desenvolvo sistemas através da minha paixão pela tecnologia,
+          contribuindo com soluções inovadoras e eficazes para desafios
+          complexos.
+        </span>
+      </div>
     </div>
   )
 }
