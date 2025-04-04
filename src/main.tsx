5dd81c0403be2c0124d07425b31afa2d13b43119
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2025-04-04 12:18:44 -0300

    feat: configure react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app.tsx'
 
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import { ExamScheduler } from './pages/exam-scheduler.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: App,
+    errorElement: <div>Oops! Something went wrong.</div>,
+  },
+  {
+    path: '/exam-scheduler',
+    Component: ExamScheduler,
+  },
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 )
---
+++ src/pages/exam-scheduler.tsx
@@ -0,0 +1,8 @@
+export function ExamScheduler() {
+  return (
+    <div>
+      <h1>Exam Scheduler</h1>
+      <p>This is the exam scheduler page.</p>
+    </div>
+  )
+}
