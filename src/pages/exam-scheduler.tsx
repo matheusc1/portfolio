35bf87050b7cc40152797d2f952c983df3e5f126
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2025-04-14 22:09:43 -0300

    feat: add header and project description to exam scheduler project page

--- src/pages/exam-scheduler.tsx
+++ src/pages/exam-scheduler.tsx
@@ -1,8 +1,24 @@
+import { LucideArrowLeft } from 'lucide-react'
+import { NavLink } from 'react-router'
+
 export function ExamScheduler() {
   return (
-    <div>
-      <h1>Exam Scheduler</h1>
-      <p>This is the exam scheduler page.</p>
+    <div className="h-dvh w-full bg-gray-500 flex flex-col items-start px-20 py-20 gap-10">
+      <div className="space-y-2">
+        <NavLink to="/" className="flex gap-2  items-center text-blue">
+          <LucideArrowLeft className="size-5" />
+          Voltar
+        </NavLink>
+        <h2 className="text-gray-100 font-title leading-title text-3xl text-shadow-lg/40">
+          Exam Scheduler
+        </h2>
+      </div>
+      <p className="text-gray-200 leading-text">
+        Exam Scheduler é um projeto Full Stack construído utilizando Node.js com
+        Fastify e React. A aplicação foi desenvolvida para facilitar o
+        agendamento de avaliações, requisitada pela UNIFAA (Universidade Federal
+        de Valença) como um projeto de formação.
+      </p>
     </div>
   )
 }
