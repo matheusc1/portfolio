4ebb59eacb40e034f400770cff77f741e5ef73a4
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2025-07-11 19:28:27 -0300

    fix: switch from browser router to hash router for GitHub Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app.tsx'
 
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { createHashRouter, RouterProvider } from 'react-router'
 import { ExamScheduler } from './pages/exam-scheduler.tsx'
 import { MyBookList } from './pages/my-book-list.tsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     Component: App,
